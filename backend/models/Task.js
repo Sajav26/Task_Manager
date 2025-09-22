c00a6d1607884561a214ed6dff7e8716d4b3aace
sajan26v <sajanthanv26@gmail.com>
2025-09-22 11:59:39 +0530

    Taskjs updated

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -11,7 +11,7 @@ const taskSchema = new mongoose.Schema({
     priority: {type:String, enum:["Low","Medium","High"], default:"Low"},
     status: {type:String, enum:["Pending","In-Progress","Completed"], default:"Pending"},
     dueDate: {type:Date, required:true},
-    assignedTo: {type: mongoose.Schema.Types.ObjectId, ref:"User", required:true},
+    assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }],
     createdBy: {type: mongoose.Schema.Types.ObjectId, ref:"User", required:true},
     attachments: [{type:String}],
     todoChecklist: [todoSchema],
