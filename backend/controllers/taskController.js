137ea2c3094c0c12a53a947eb9fa94b907475bb5
sajan26v <sajanthanv26@gmail.com>
2025-09-22 12:18:00 +0530

    task controller updated

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -170,7 +170,26 @@ const deleteTask = async (req, res) => {
 //! @access Private
 const updateTaskStatus = async (req, res) => {
     try{
+        const task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).json({ message: 'Task not found' });
 
+        const isAssigned = task.assignedTo.some(
+            (userId) => userId.toString() === req.user._id.toString()
+        );
+
+        if (!isAssigned && req.user.role !== 'admin') {
+            return res.status(403).json({ message: 'You are not authorized to update the status of this task' });
+        }
+
+        task.status = req.body.status || task.status;
+
+        if (task.status === 'Completed') {
+            task.todoChecklist = task.todoChecklist.map(item => ({ ...item, completed: true }));
+            task.progress = 100;
+        }
+
+        await task.save();
+        res.json({ message: 'Task status updated successfully', task });
     }catch(error){
         res.status(500).json({ message: 'Server Error', error: error.message });
     }
@@ -181,7 +200,40 @@ const updateTaskStatus = async (req, res) => {
 //! @access Private
 const updateTaskChecklist = async (req, res) => {
     try{
+        const { todoChecklist } = req.body;
+        const task = await Task.findById(req.params.id);
 
+        if (!task) return res.status(404).json({ message: 'Task not found' });
+
+        if (!task.assignedTo.includes(req.user._id) && req.user.role !== 'admin') {
+            return res
+            .status(403)
+            .json({ message: 'You are not authorized to update the checklist of this task' });
+        }
+
+        task.todoChecklist = todoChecklist; // Replace with updated checklist
+
+        // Auto-update progres based on checklist completion
+        const completedCount = todoChecklist.filter(item => item.completed).length;
+        const totalItems = todoChecklist.length;
+        task.progress = totalItems> 0 ? Math.round((completedCount / totalItems) * 100) : 0;
+
+        // Auto-mark task as completed if all checked
+        if (task.progress === 100) {
+            task.status = 'Completed';
+        }else if (task.progress > 0){
+            task.status = 'In-Progress';
+        }else{
+            task.status = 'Pending';
+        }
+
+        await task.save();
+        const updatedTask = await Task.findById(req.params.id).populate(
+            'assignedTo',
+            'name email profileImageUrl'
+        );
+
+        res.json({ message: 'Task checklist updated successfully', task: updatedTask });
     }catch(error){
         res.status(500).json({ message: 'Server Error', error: error.message });
     }
